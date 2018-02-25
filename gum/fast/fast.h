fdd8eb17b252ec6a3ac0af67cbe73120d40ba791
Mars Saxman <mars@redecho.org>
2018-02-24 19:09:00 -0800

    add command line flags to include "cairo.h" and link against libcairo

--- gum/fast/fast.h
+++ gum/fast/fast.h
@@ -5,11 +5,3 @@ typedef struct {
   void *ctx;
 } PycairoContext;
 
-void cairo_move_to (void *cr, double x, double y);
-void cairo_line_to (void *cr, double x, double y);
-void cairo_rectangle(void *cr, double x, double ymin,
-                     double w, double h);
-void cairo_set_source_rgb (void *cr, double r, double g, double b);
-void cairo_stroke (void *cr);
-void cairo_fill (void *cr);
-void cairo_set_line_width(void *cr, double w);
